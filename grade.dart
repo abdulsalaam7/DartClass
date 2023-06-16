3b31b8fd5c9e562135c6c60950432d7a5b6e27b5
abdulsalaam7 <abdulsalaammoosani@gmail.com>
2023-06-17 00:59:43 +0500

    Work Complete

---
+++ grade.dart
@@ -0,0 +1,19 @@
+void main() {
+  num percentage = 83;
+
+  if (percentage >= 90) {
+    print("A+");
+  } else if (percentage >= 80) {
+    print("A");
+  } else if (percentage >= 70) {
+    print("B");
+  } else if (percentage >= 60) {
+    print("C");
+  } else if (percentage >= 50) {
+    print("D");
+  } else if (percentage >= 40) {
+    print("E");
+  } else {
+    print("Fail");
+  }
+}
